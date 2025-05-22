7aeae05d5930a29c01e77066a0d345b44f9ce954
PhiB64 <philippebarbosa64@gmail.com>
2025-05-22 18:02:25 +0200

    Ajout script Js fetch et création des cartes à la volée + début CSS

---
+++ script.js
@@ -0,0 +1,36 @@
+fetch("https://demo.theeventscalendar.com/wp-json/tribe/events/v1/events")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data.events);
+
+    data.events.forEach((event) => {
+      const scrollEventsContainer = document.getElementById(
+        "scrollEventsContainer"
+      );
+      const eventCard = document.createElement("div");
+      eventCard.id = "eventCard";
+
+      const detailsButton = document.createElement("button");
+      detailsButton.id = "detailsButton";
+      detailsButton.textContent = "Voir détails";
+
+      const planningButton = document.createElement("button");
+      planningButton.id = "planningButton";
+      planningButton.textContent = "Ajouter au planning";
+
+      const eventTitle = document.createElement("h3");
+      eventTitle.id = "eventtitle";
+      eventTitle.textContent = event.title;
+
+      const eventDate = document.createElement("p");
+      eventDate.id = "eventDate";
+      eventDate.textContent = event.start_date.split(" ")[0];
+
+      scrollEventsContainer.appendChild(eventCard);
+      eventCard.appendChild(eventTitle);
+      eventCard.appendChild(eventDate);
+      eventCard.appendChild(detailsButton);
+      eventCard.appendChild(planningButton);
+    });
+  })
+  .catch((error) => console.error("Erreur API :", error));
