4f28249123a296c51f4a9137a6ae9d77c95f56fd
PhiB64 <philippebarbosa64@gmail.com>
2025-05-23 16:58:54 +0200

    Clone cartes dans le planning avec maj des boutons

--- script.js
+++ script.js
@@ -15,8 +15,12 @@ fetch("https://demo.theeventscalendar.com/wp-json/tribe/events/v1/events")
       detailsButton.textContent = "Voir détails";
 
       const planningButton = document.createElement("button");
-      planningButton.id = "planningButton";
+      planningButton.className = "planningButton";
       planningButton.textContent = "Ajouter au planning";
+
+      const removeButton = document.createElement("button");
+      removeButton.className = "removeButton";
+      removeButton.textContent = "Retirer du planning";
 
       const eventTitle = document.createElement("h3");
       eventTitle.id = "eventTitle";
@@ -45,12 +49,80 @@ fetch("https://demo.theeventscalendar.com/wp-json/tribe/events/v1/events")
         const modalDate = document.getElementById("modalDate");
         const modalAdress = document.getElementById("modalAdress");
         const modalDescription = document.getElementById("modalDescription");
-        modalTitle.textContent = event.title;
+        modalTitle.innerHTML = event.title;
         modalDate.textContent = newFormatDate;
         modalAdress.textContent = `Lieu: ${event.venue.slug}`;
         modalDescription.innerHTML = event.description;
+        modal.style.display = "block";
+
+        const closeButton = document.getElementById("closeModal");
+        closeButton.addEventListener("click", () => {
+          modal.style.display = "none";
+        });
+      });
+
+      planningButton.addEventListener("click", () => {
+        const scrollPlanningContainer = document.getElementById(
+          "scrollPlanningContainer"
+        );
+
+        const existingEvent = scrollPlanningContainer.querySelector(
+          `[data-event-id="${event.id}"]`
+        );
+
+        if (!existingEvent) {
+          const eventCardClone = eventCard.cloneNode(true);
+
+          eventCardClone.removeAttribute("id");
+          eventCardClone.setAttribute("data-event-id", event.id);
+          eventCardClone.className = "eventCardClone";
+
+          eventCardClone.querySelector(".planningButton").remove();
+          eventCardClone.appendChild(removeButton);
+          removeButton.addEventListener("click", () => {
+            eventCardClone.remove();
+          });
+
+          const detailsButtonClone =
+            eventCardClone.querySelector(".detailsButton");
+          detailsButtonClone.addEventListener("click", () => {
+            const modalTitle = document.getElementById("modalTitle");
+            const modalDate = document.getElementById("modalDate");
+            const modalAdress = document.getElementById("modalAdress");
+            const modalDescription =
+              document.getElementById("modalDescription");
+
+            modalTitle.innerHTML = event.title;
+            modalDate.textContent = newFormatDate;
+            modalAdress.textContent = `Lieu: ${event.venue.slug}`;
+            modalDescription.innerHTML = event.description;
+
+            document.getElementById("modal").style.display = "block";
+
+            const closeButton = document.getElementById("closeModal");
+            closeButton.addEventListener("click", () => {
+              modal.style.display = "none";
+            });
+          });
+
+          scrollPlanningContainer.appendChild(eventCardClone);
+        }
       });
     });
   })
 
   .catch((error) => console.error("Erreur API :", error));
+
+const darkModeImage = document.querySelector(".darkModeImage");
+
+if (localStorage.getItem("darkMode") === "enabled") {
+  document.body.classList.add("dark-mode");
+}
+
+darkModeImage.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+  localStorage.setItem(
+    "darkMode",
+    document.body.classList.contains("dark-mode") ? "enabled" : "disabled"
+  );
+});
